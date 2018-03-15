29abc1c85a3126d0531b23321dc3f700ad5e6a41
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-15 12:28:26 +0530

    Children

--- public/script.ts
+++ public/script.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {
     Component,NgModule,Input,Output,EventEmitter,ViewEncapsulation,SimpleChanges,
     OnChanges,OnInit,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,
-    OnDestroy
+    OnDestroy, ViewChild , ViewChildren, QueryList, ContentChild, ContentChildren, ElementRef
 } from '@angular/core';
 
 class Player{
@@ -52,46 +52,46 @@ class PlayerComponent
 {
     @Input() data: Player;
 
-    constructor() {
-        console.log(`new - data is ${this.data}`);
-      }
+    // constructor() {
+    //     console.log(`new - data is ${this.data}`);
+    //   }
     
-      ngOnChanges(changes: SimpleChanges) {
-        console.log(`ngOnChanges - data is ${this.data}`);
-        for (let key in changes) {
-            console.log(`${key} changed.
-            Current: ${changes[key].currentValue}.
-            Previous: ${changes[key].previousValue}`);
-          }
-      }
+    //   ngOnChanges(changes: SimpleChanges) {
+    //     console.log(`ngOnChanges - data is ${this.data}`);
+    //     for (let key in changes) {
+    //         console.log(`${key} changed.
+    //         Current: ${changes[key].currentValue}.
+    //         Previous: ${changes[key].previousValue}`);
+    //       }
+    //   }
     
-      ngOnInit() {
-        console.log(`ngOnInit  - data is ${this.data}`);
-      }
+    //   ngOnInit() {
+    //     console.log(`ngOnInit  - data is ${this.data}`);
+    //   }
     
-      ngDoCheck() {
-        console.log("ngDoCheck")
-      }
+    //   ngDoCheck() {
+    //     console.log("ngDoCheck")
+    //   }
     
-      ngAfterContentInit() {
-        console.log("ngAfterContentInit");
-      }
+    //   ngAfterContentInit() {
+    //     console.log("ngAfterContentInit");
+    //   }
     
-      ngAfterContentChecked() {
-        console.log("ngAfterContentChecked");
-      }
+    //   ngAfterContentChecked() {
+    //     console.log("ngAfterContentChecked");
+    //   }
     
-      ngAfterViewInit() {
-        console.log("ngAfterViewInit");
-      }
+    //   ngAfterViewInit() {
+    //     console.log("ngAfterViewInit");
+    //   }
     
-      ngAfterViewChecked() {
-        console.log("ngAfterViewChecked");
-      }
+    //   ngAfterViewChecked() {
+    //     console.log("ngAfterViewChecked");
+    //   }
     
-      ngOnDestroy() {
-        console.log("ngOnDestroy");
-      }
+    //   ngOnDestroy() {
+    //     console.log("ngOnDestroy");
+    //   }
 
 }
 
@@ -101,14 +101,30 @@ class PlayerComponent
 })
 class PlayerListComponent{
     
-    players : Player[] = [];
+    players : Player[] = [ new Player("Lionel Messi 1","FC Barcelona") , new Player("Lionel Messi 2","FC Barcelona")];
 
-    addPlayer(  ) {
-        this.players.unshift( new Player("Lionel Messi","FC Barcelona") );
+    @ViewChild(PlayerComponent) playerViewChild : PlayerComponent;
+    @ViewChildren(PlayerComponent) playerViewChildren: QueryList<PlayerComponent>;
+    @ViewChild("header") headerEl: ElementRef;
+    @ContentChild(PlayerComponent) playerContentChild: PlayerComponent;
+
+    constructor()
+    {
+        console.log(`new - playerViewChild is ${JSON.stringify(this.playerViewChild)}`);
     }
 
-    deletePlayer() {
-        this.players = [];
+    ngAfterContentInit() {
+        console.log(`ngAfterContentInit - jokeContentChild is ${this.playerContentChild}`);
+    }
+
+    ngAfterViewInit() {
+        console.log(`ngAfterViewInit - playerViewChild is ${JSON.stringify(this.playerViewChild)}`);
+        
+        let jokes: PlayerComponent[] = this.playerViewChildren.toArray(); 
+        console.log(jokes);
+
+        console.log(`ngAfterViewInit - headerEl is ${this.headerEl}`);
+        this.headerEl.nativeElement.textContent = "Best Joke Machine";
     }
     
 }
@@ -119,6 +135,9 @@ class PlayerListComponent{
     templateUrl: './app-component.html' 
 })
 class AppComponent {
+
+    player : Player = new Player("Lionel Messi 3","FC Barcelona");
+
 }
 
 @NgModule({
