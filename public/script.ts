e90679c7a9fbd1f53ce343faefcb0f544cc24e65
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-15 17:37:20 +0530

    Host binder , listener

--- public/script.ts
+++ public/script.ts
@@ -5,7 +5,7 @@ import {
     Component,NgModule,Input,Output,EventEmitter,ViewEncapsulation,SimpleChanges,
     OnChanges,OnInit,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,
     OnDestroy, ViewChild , ViewChildren, QueryList, ContentChild, ContentChildren, ElementRef,
-    Directive, Renderer
+    Directive, Renderer , HostListener, HostBinding
 } from '@angular/core';
 
 class Player{
@@ -48,10 +48,26 @@ class Player{
     selector : "[ccCardHover]"
 })
 class CardHoverDirective {
+
+    @HostBinding('class.card-outline-primary')private ishovering: boolean;
+
     constructor( private el : ElementRef , private renderer : Renderer ){
         // el.nativeElement.style.backgroundColor = 'grey';
-        renderer.setElementStyle( el.nativeElement , 'backgroundColor' , 'gray');
+        // renderer.setElementStyle( el.nativeElement , 'backgroundColor' , 'gray');
     }
+
+    @HostListener('mouseover') onMouseOver() { 
+        let part = this.el.nativeElement.querySelector('.card-text') 
+        this.renderer.setElementStyle(part, 'display', 'block'); 
+        this.ishovering = true;
+    }
+
+    @HostListener('mouseout') onMouseOut() {
+        let part = this.el.nativeElement.querySelector('.card-text');
+        this.renderer.setElementStyle(part, 'display', 'none');
+        this.ishovering = false;
+    }
+
 }
 
 
